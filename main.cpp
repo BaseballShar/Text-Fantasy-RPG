a64dc6c76d0acef7f789bb48546936be8b8a3c5e
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-03-09 16:03:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// ENGG 1340 Group 137 Project
+//
+// Developer: Chan Tsz Ho  (3035785738)
+//            Chan Yuk Yin (3035786574)
+//
+// Description: This is a text-based role-playing game which is set in 
+// a dark medieval fantasy world. You the player will act as an adventurer 
+// in the game. By acquiring strength and powerful equipment through combating
+// monsters spread throughout the world, player will eventually be able to 
+// fight the Monster King and save mankind to win the game.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  return 0;
+}
