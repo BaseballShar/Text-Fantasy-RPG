d48a3891013f89209025dddd6857a1daa1e544f4
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-03-09 17:37:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,47 +16,47 @@ using namespace std;
 // functions to set and print status of the player
 class Player
 {
-  public :
-    bool death = false ; // Check the player is dead or not
-    string name ; // To save the player name
-    string career ; // To store the job selection of the player
+  public:
+    bool death = false; // Check the player is dead or not
+    string name; // To save the player name
+    string career; // To store the job selection of the player
 
-    long long hp_basic ; //variables for the hp
-    double item_hp_multiplier = 1 ; 
-    long long hp_actual ;
+    long long hp_basic; //variables for the hp
+    double item_hp_multiplier = 1; 
+    long long hp_actual;
 
-    long long str_basic ; //variables for the strength
-    double item_str_multiplier = 1 ;
-    long long str_actual ;
+    long long str_basic; //variables for the strength
+    double item_str_multiplier = 1;
+    long long str_actual;
 
-    long long def_basic ; //variables for the defense
-    double item_def_multiplier = 1 ;
-    long long def_actual ;
+    long long def_basic; //variables for the defense
+    double item_def_multiplier = 1;
+    long long def_actual;
 
-    long long agi_basic ;  //variables for the agility
-    double item_agi_multiplier = 1 ;
-    long long agi_actual ;
+    long long agi_basic;  //variables for the agility
+    double item_agi_multiplier = 1;
+    long long agi_actual;
 
-    int level = 1 ; //variables for storing player xp levels and money
-    int exp = 0 ;
-    double xp_multiplier ;
-    int skill_pt = 0 ;
-    int money = 0 ;
-    int money_multiplier ;
+    int level = 1; //variables for storing player xp levels and money
+    int exp = 0;
+    double xp_multiplier;
+    int skill_pt = 0;
+    int money = 0;
+    int money_multiplier;
     
     // Purpose: This function is to initialize the status of the player
-    void set_status(long long hp_basic, long long str_basic, 
+    void SetStatus(long long hp_basic, long long str_basic, 
                     long long def_basic, long long agi_basic, string career) {   
-      this -> hp_basic = hp_basic ;
-      this -> str_basic = str_basic ;
-      this -> def_basic = def_basic ;
-      this -> agi_basic = agi_basic ;
-      this -> career = career ;
+      this -> hp_basic = hp_basic;
+      this -> str_basic = str_basic;
+      this -> def_basic = def_basic;
+      this -> agi_basic = agi_basic;
+      this -> career = career;
     }
 
     // Purpose: This function is to print the status of the player
-    void print_status() {
-      cout << name << " hp: "<< hp_actual << " strength: " << str_actual 
+    void PrintStatus() {
+      cout << name << " hp: " << hp_actual << " strength: " << str_actual 
       << " defense: " << def_actual << " and agility: " << agi_actual << endl ;
     }
 };
@@ -66,20 +66,22 @@ void DisplayWelcomeMenu() {
 } 
 
 bool IsOldPlayer() {   
-  int has_played ;
+  int has_played;
+
   cout << "Have you played this game before" << endl;
-  cout << "Enter 1 if you haven't , 2 if you have : " ;
+  cout << "Enter 1 if you haven't , 2 if you have : ";
   cin >> has_played ;
 
-  switch(has_played)
-  {
+  switch(has_played) {
     case 1:
       return false;
 
     case 2:
       return true;
+
+    default:
+      return false;
   }   
-  return false;
 }
 
 int main() {
