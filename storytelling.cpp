66da4c62b392afb7c7de7b2b6c1d2c5bcf73b024
Chan Yuk Yin, York <79845066+yyorkchan@users.noreply.github.com>
2021-03-09 18:22:27 +0800

    Add files via upload

---
+++ storytelling.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <string>
+void PrintStory(){
+  string story = "You were a hardworking programmer in an international IT\n"
+  "company,you had contributed to the company with all your heart and soul.\n"
+  "However, you recently discovered that your boss had been stealing your idea\n"
+  "and presenting it to the central which brought him wealth and fame.\n"
+  "You were so outraged that you grabbed a keyboard nearby and smashed it\n"
+  "against your boss.\n\n"
+  "Not knowing why you missed it!\n"
+  "The keyboard crashed onto the monitor and the electrical wires inside\n"
+  "were in contact with your hand. It was too late to retract your hands.\n"
+  "Suddenly, a blue portal emerged near the desk and you were teleported to\n"
+  "a strange new world.\n\n"   
+  "And now! You are destinated to be the hero of this world!\n"
+  "Fight the three most horrible Monsters to take their maps!\n"
+  "The maps lead you to fight the King Of Boss \"Infinite Comments Client\"!\n"
+  "The world needs your power!\n";
+  cout << story << endl;
+}
