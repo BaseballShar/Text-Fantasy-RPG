0da7a40dd650063dda4b05aa99ef27f226d74ad3
Chan Yuk Yin, York <79845066+yyorkchan@users.noreply.github.com>
2021-03-13 13:21:02 +0800

    Upload the header file of skill points

---
+++ skill_points.h
@@ -0,0 +1,14 @@
+#ifndef SKILL_POINT_H
+#define SKILL_POINT_H
+
+#include <iostream>
+#include "character.h"
+using namespace std;
+
+// Purpose: Skill point system
+void SkillPoint(Player &player);
+
+// Purpose: update the level status of the player
+void LevelUpdate(Player &player);
+
+#endif
