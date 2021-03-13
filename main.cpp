641f3d33111d8e3c1f6778ead8d0f5dcc368dcec
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-03-13 12:27:15 +0800

    Moved Player and Monster class into separate files. Combat parts are also moved into separate files

--- main.cpp
+++ main.cpp
@@ -13,76 +13,10 @@
 #include <fstream>
 #include <cmath>
 #include <string>
-#include <thread>
-#include <chrono>
+
+#include "combat.h"
+#include "character.h"
 using namespace std;
-
-// Purpose: For storing various status of the player and providing useful 
-// functions to set and print status of the player
-class Player
-{
-  public:
-    bool death = false; // Check the player is dead or not
-    string name; // To save the player name
-    string career; // To store the job selection of the player
-
-    long long hp_basic; // Variables for the hp
-    double item_hp_multiplier = 1; 
-    long long hp_actual;
-
-    long long str_basic; // Variables for the strength
-    double item_str_multiplier = 1;
-    long long str_actual;
-
-    long long def_basic; // Variables for the defense
-    double item_def_multiplier = 1;
-    long long def_actual;
-
-    long long agi_basic;  // Variables for the agility
-    double item_agi_multiplier = 1;
-    long long agi_actual;
-
-    int level = 1; // Variables for storing player xp levels and money
-    int exp = 0;
-    double xp_multiplier;
-    int skill_pt = 0;
-    int money = 0;
-    int money_multiplier;
-    
-    // Input: Basic status and career of the player
-    // Purpose: This function is to initialize the status of the player
-    void SetStatus(long long hp_basic, long long str_basic, 
-                    long long def_basic, long long agi_basic, string career) {   
-      this -> hp_basic = hp_basic;
-      this -> str_basic = str_basic;
-      this -> def_basic = def_basic;
-      this -> agi_basic = agi_basic;
-      this -> career = career;
-    }
-
-    // Purpose: This function is to print the status of the player
-    void PrintStatus() {
-      cout << name << " hp: " << hp_actual << " strength: " << str_actual 
-      << " defense: " << def_actual << " and agility: " << agi_actual << endl;
-    }
-};
-
-// Purpose: For storing various status of the monster and providing useful 
-// functions to print status of the monster
-class Monster {
-  public:
-    int level;
-    int hp; 
-    int str; 
-    int def; 
-    int agi;
-    
-    // Purpose: Printing out the monster status
-    void PrintStatus() {
-      cout << "Monster  hp: "<< hp << " strength: " << str << " defense: " 
-      << def << " and agility: " << agi << endl;
-    }
-} ;
 
 // Purpose: Print out a line of user specified symbol and length
 void Line(char symbol, int noOfSym = 85) {   
@@ -91,11 +25,6 @@ void Line(char symbol, int noOfSym = 85) {
     output.push_back(symbol) ;
   }
   cout << output << endl ;
-}
-
-// Purpose: Pause the game for (time) miliseconds
-void Pause(int time) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(time));
 }
 
 // Purpose: Print out the game story
@@ -183,8 +112,10 @@ void LoadGame(Player &player) {
 // Purpose: Function to get the name of the player
 void GetName(Player &player) {   
   string name;
+
   cout << "What is your name adventurer ? ";
-  cin >> name;
+  getline(cin, name);
+  getline(cin, name);
   cout << "Welcome " << name << endl;
   player.name = name;
 }
@@ -235,180 +166,9 @@ void DisplayWelcomeMenu(Player &player) {
   } else {  
     PrintStory(); 
     GetName(player);
-    GetCareer(500, player); // 100 ms pause
+    GetCareer(500, player); // 500 ms pause
   }
 } 
-
-Monster CombatLevelSelection(Player player) {
-  int monster_level;
-  Monster monster;
-  bool confirm_choice = false;
-    
-    // Monster level selection
-  while (confirm_choice == false) {   
-    cout << "What level of monster you want to fight ? ";
-    cin >> monster_level;
-    
-    monster.level = monster_level;
-    monster.hp = 500 * monster_level;
-    monster.str = 100 * log2(monster_level + 1);
-    monster.def = 50 *  log2(monster_level + 1);
-    monster.agi = 50 * log2(monster_level + 1);
-    cout << "Are you sure you want to fight level " 
-    << monster_level << " monster with " << endl;
-    monster.PrintStatus();
-    player.PrintStatus();
-
-    cout << "YES[1] \t No[0] " << endl;
-    cin >> confirm_choice;
-    }
-
-    return monster;
-}
-
-// Purpose: Function for calculating the attack modifier 
-// based on strength and defense
-double CombatMultipler(int attack_str, int defend_def) {
-  double ratio = attack_str * 1.0 / defend_def;
-  return 4 / (1 + exp(-ratio)) - 2;
-}
-
-// Purpose: To determine whether a hit is successful or not
-// Mechanism : Create a bound of a randomly generate number by the ratio of agility of both sides
-// the higher the ratio, the more large the bound , hence a hit is more likely
-bool CombatIsHit(int attack_agi, int defend_agi) {    
-  double max = attack_agi * 100 / defend_agi;
-
-  if ( rand() % 100 + 1 <= max ) {
-    return true;
-  }
-  return false;
-}
-
-// Purpose: To calculate health reduction of monster(value of the attack) 
-// by considering status(strength, defense and agility) of both sides
-void CombatPlayerAttack(Player &player, Monster &monster) {
-  Pause(500);
-
-  cout << endl;
-  cout << "It is the player's turn to attack" << endl; 
-
-  if (CombatIsHit(player.agi_actual * player.item_agi_multiplier,
-      monster.agi)) {   
-    int player_att = player.str_actual * player.item_str_multiplier 
-    * CombatMultipler(player.str_actual
-    * player.item_str_multiplier, monster.def);
-
-    monster.hp -= player_att;
-
-    cout << "You have dealt " << player_att 
-    << " damage to the monster" << endl;
-
-    cout << player.name << " HP : " << player.hp_actual << " Monster HP: " 
-    << monster.hp << endl;
-  } else {
-    cout << "Monster evaded your attack" << endl;
-    }
-}
-
-// Purpose: To calculate health reduction of player(value of the attack) 
-// by considering status(strength, defense and agility) of both sides
-void CombatMonsterAttack(Player &player, Monster &monster) {
-    Pause(500);
-    
-    cout << "It is the Monster's turn to attack" << endl; 
-
-    if (CombatIsHit(monster.agi, player.agi_actual 
-        * player.item_agi_multiplier)) {   
-      int monster_att = monster.str 
-      * CombatMultipler(monster.str, player.def_actual 
-      * player.item_def_multiplier);
-
-      player.hp_actual -= monster_att;
-      
-      cout << "Monster have dealt " << monster_att 
-      << " damage to you" << endl;
-
-      cout << player.name << " HP : " << player.hp_actual 
-      << " Monster HP: " << monster.hp << endl;
-
-    } else {
-      cout << "You have evaded monster's attack" << endl;
-    }
-}
-
-// Purpose: To determine the outcome of the combat with the monster
-// Experience point and money gained is calculated using the level
-// of monster that the player defeated
-void CombatReward(Player &player, int combat_result) {
-  if (combat_result != -1) {
-    player.exp += 100 * combat_result;
-    player.money += 50 * combat_result;
-    cout << "You have acquried " << "$" << 100 * combat_result  << endl ;
-    cout << "You have acquried " << 100 * combat_result 
-    << " XP points" << endl ;
-
-  } else { 
-    player.death = true ;
-    }
-}
-
-// Purpose: To be in charge of the whole combat situation
-// From monster creation, combating, to reward distribution
-int Combat (Player &player/*, Shop shop*/) {   
-    //bool auto_combat = player.setting[0].second ;
-    
-    // Backup character data 
-    Player player_backup = player; // Disallow status to changing while recording item comsumption
-    
-    // Update player status 
-    //player_equipment_effect(player, shop) ;
-
-    // Creation of monster
-    Monster monster = CombatLevelSelection(player);
-    
-    // Fighting with monster   
-    while (player.hp_actual > 0 && monster.hp > 0) {   
-      /*if (auto_combat == false) {   
-        int player_option;
-        cout << "What do you want to do next ? " << endl;
-        cout << "Enter 1 for normal attack  " << endl;
-        cout << "Enter 2 for using items  " << endl;
-        cin >> player_option;
-
-        switch(player_option) {
-          case 1 :
-            CombatPlayerAttack (player, monster);
-            break ;
-          case 2 :
-            //combat_player_use_item (player, player_backup);
-            break;
-        }
-
-      } else {*/
-        CombatPlayerAttack(player, monster);
-        //} 
-
-        if (!(player.hp_actual > 0 && monster.hp > 0)) {
-          break;
-        }
-
-        CombatMonsterAttack(player, monster);      
-    }
-    
-    // Combat outcome
-    if (player.hp_actual > 0) {   
-        cout << "You have defeated the monster !!! " << endl;
-
-        // regenerate character status
-        player = player_backup;
-
-        return monster.level;
-    } else {   
-        cout << "You have been defeated " << endl;
-        return -1;
-    }
-}
 
 // Purpose: Function to get the action take by the player
 void GetAction(Player &player/*, Shop shop*/) {   
@@ -470,6 +230,7 @@ void PlayerEquipmentEffect (Player &player/*, Shop shop*/) {
   player.def_actual = player.def_basic;
   player.agi_actual = player.agi_basic;
 }
+
 //main function
 int main() {
   Player player;
@@ -478,14 +239,12 @@ int main() {
   DisplayWelcomeMenu(player);
 
   // Playing session
-  while (player.death == false)
-    {   
-        PlayerEquipmentEffect(player);
-        player.PrintStatus();
-        GetAction(player/*, shop*/) ;
-        /*if (player.death == false)
-            {level_update(player) ;}*/
-    }
+  while (player.death == false) {   
+    PlayerEquipmentEffect(player);
+    player.PrintStatus();
+    GetAction(player/*, shop*/);
+    /*if (player.death == false)
+        {level_update(player) ;}*/
+  }
     return 0;
 }
-
