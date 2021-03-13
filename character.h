a37214987f6add7b19b6a7a72bfccdb8d5b7f67a
Chan Yuk Yin, York <79845066+yyorkchan@users.noreply.github.com>
2021-03-13 12:31:45 +0800

    upload the header file of character

---
+++ character.h
@@ -0,0 +1,63 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Purpose: For storing various status of the player and providing useful 
+// functions to set and print status of the player
+class Player
+{
+  public:
+    bool death = false; // Check the player is dead or not
+    string name; // To save the player name
+    string career; // To store the job selection of the player
+
+    long long hp_basic; // Variables for the hp
+    double item_hp_multiplier = 1; 
+    long long hp_actual;
+
+    long long str_basic; // Variables for the strength
+    double item_str_multiplier = 1;
+    long long str_actual;
+
+    long long def_basic; // Variables for the defense
+    double item_def_multiplier = 1;
+    long long def_actual;
+
+    long long agi_basic;  // Variables for the agility
+    double item_agi_multiplier = 1;
+    long long agi_actual;
+
+    int level = 1; // Variables for storing player xp levels and money
+    int exp = 0;
+    double xp_multiplier;
+    int skill_pt = 0;
+    int money = 0;
+    int money_multiplier;
+    
+    // Input: Basic status and career of the player
+    // Purpose: This function is to initialize the status of the player
+    void SetStatus(long long hp_basic, long long str_basic, 
+                    long long def_basic, long long agi_basic, string career);
+
+    // Purpose: This function is to print the status of the player
+    void PrintStatus();
+};
+
+// Purpose: For storing various status of the monster and providing useful 
+// functions to print status of the monster
+class Monster {
+  public:
+    int level;
+    int hp; 
+    int str; 
+    int def; 
+    int agi;
+    
+    // Purpose: Printing out the monster status
+    void PrintStatus();
+};
+
+#endif
