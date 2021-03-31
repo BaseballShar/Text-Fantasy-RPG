5df44c8ea580af670cbdb2e63ce05adf2cc827dd
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-03-31 21:57:53 +0800

    Added definitions for equipment status printing function

--- character.cpp
+++ character.cpp
@@ -1,4 +1,6 @@
+#include <sstream>
 #include "character.h"
+#include "list.h"
     
 // Input: Basic status and career of the player
 // Purpose: This function is to initialize the status of the player
@@ -15,6 +17,60 @@ void Player::SetStatus(long long hp_basic, long long str_basic,
 void Player::PrintStatus() {
   cout << name << " HP: " << hp_actual << " STR: " << str_actual 
   << " DEF: " << def_actual << " AGI: " << agi_actual << endl;
+}
+
+// Purpose: This function is to print the equipments of the player
+void Player::PrintEquipment() {
+  istringstream weapon_str(weapon);
+  istringstream armour_str(armour);
+
+  string weapon_name = "";
+  string weapon_hp_bonus = "";
+  string weapon_str_bonus = "";
+  string weapon_def_bonus = "";
+  string weapon_agi_bonus = "";
+
+  string armour_name = "";
+  string armour_hp_bonus = "";
+  string armour_str_bonus = "";
+  string armour_def_bonus = "";
+  string armour_agi_bonus = "";
+  
+
+  if (weapon == "") {
+    cout << "You have not purchased any weapon" << endl;
+  } else {
+    weapon_str >> weapon_name;
+    weapon_str >> weapon_hp_bonus;
+    weapon_str >> weapon_str_bonus;
+    weapon_str >> weapon_def_bonus;
+    weapon_str >> weapon_agi_bonus;
+
+    cout << "Currently equiped weapon:" << endl;
+    cout << "Name: " << weapon_name << " HP: +" << (stoi(weapon_hp_bonus) - 1) * 100 << "%";
+    cout << " STR: +" << (stoi(weapon_str_bonus) - 1) * 100 << "%";
+    cout << " DEF: +" << (stoi(weapon_def_bonus) - 1) * 100 << "%";
+    cout << " AGI: +" << (stoi(weapon_agi_bonus) - 1) * 100 << "%";
+
+    
+  }
+  if (armour == "") {
+    cout << "You have not purchased any armour" << endl;
+  } else {
+    armour_str >> armour_name;
+    armour_str >> armour_hp_bonus;
+    armour_str >> armour_str_bonus;
+    armour_str >> armour_def_bonus;
+    armour_str >> armour_agi_bonus;
+    
+    cout << "Curently equiped wamour:" << endl;
+    cout << "Name: " << weapon_name << " HP: +" << (stoi(armour_hp_bonus) - 1) * 100 << "%";
+    cout << " STR: +" << (stoi(armour_str_bonus) - 1) * 100 << "%";
+    cout << " DEF: +" << (stoi(armour_def_bonus) - 1) * 100 << "%";
+    cout << " AGI: +" << (stoi(armour_agi_bonus) - 1) * 100 << "%";
+
+  }  
+  
 }
 
 // Purpose: Printing out the monster status
