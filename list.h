95b12c469a86dcfd2a35c1ff866db0224d34e91e
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-04-02 15:33:13 +0800

    Removed Irrelevant functions and added useful functions
    Functions added include:
    Insert: To add element at the index specified
    Find: Returns the index of the requested element
    DeleteAll: Delete all elements from the list

--- list.h
+++ list.h
@@ -16,29 +16,30 @@ struct List {
   Node *tail = 0;
   int len = 0;
 
-  string first();
+  // Purpose: To add (str) to the back of the list
+  void Append(string str);
 
-  string last();
-
-  void AppendBack(string str);
-
+  // Purpose: To add (str) to the front of the list
   void AppendFront(string str);
-
-  void PrintForward();
-
-  void PrintBackward();
 
   // Purpose: Returns elements at the ith index positions
   string Extract(int index);
 
+  // Purpose: To delete the element at index (index)
   void DeleteIndex(int index);
 
+  // Purpose: To delete the element of value (value)
   void DeleteValue(string value);
 
+  // Purpose: To erase all elements in the list
+  void DeleteAll();
+
+  // Purpose: To find and return the index of element of value (value)
+  int Find(string value);
+
+  // Purpose: To insert the element of value (value) at the index position
+  void Insert(int index, string value);
+
 };
-
-void SortAsc(List &list);
-
-void SortDsc(List &list);
 
 #endif
