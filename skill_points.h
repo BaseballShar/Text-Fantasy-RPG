a0aee7f4e44f125ae6748ede7498f8a627fe0bff
Chan Yuk Yin, York <79845066+yyorkchan@users.noreply.github.com>
2021-04-02 15:52:23 +0800

    Updated variable of skill point system

--- skill_points.h
+++ skill_points.h
@@ -3,12 +3,25 @@
 
 #include <iostream>
 #include "character.h"
+#include "useful_function.h"
 using namespace std;
 
-// Purpose: Skill point system
+// Purpose: To assign skill points to HP
+void UpgradeHP(Player &player);
+
+// Purpose: To assign skill points to STR
+void UpgradeSTR(Player &player);
+
+// Purpose: To assign skill points to DEF
+void UpgradeDEF(Player &player);
+
+// Purpose: To assign skill points to AGI
+void UpgradeAGI(Player &player);
+
+// Purpose: To be in charge of the skill point system
 void SkillPoint(Player &player);
 
-// Purpose: update the level status of the player
+// Purpose: To update the level status of the player
 void LevelUpdate(Player &player);
 
 #endif
