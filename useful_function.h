de16d6634f505bd8736771a3f2abaf9448273df4
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-04-02 15:18:46 +0800

    Added function for determining whether a input is consist of numbers only
    It can be used to verify the validity of inputs

---
+++ useful_function.h
@@ -0,0 +1,10 @@
+#ifndef USEFUL_FUNCTION_H
+#define USEFUL_FUNCTION_H
+
+#include <string>
+using namespace std;
+
+// Purpose: To check whether an input string is consisted of numbers only
+bool IsNum(string value);
+
+#endif
