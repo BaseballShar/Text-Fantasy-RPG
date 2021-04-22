daf29262cb333c971a5a14fcb9e1b3874d270013
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2021-04-22 21:57:42 +0800

    Improved IsNum function

--- useful_function.cpp
+++ useful_function.cpp
@@ -3,7 +3,7 @@
 // Purpose: To check whether an input string is consisted of numbers only
 bool IsNum(string value) {
   for (int i = 0; i < value.length(); i++) {
-    if (!isnumber(value[i]))
+    if (!isdigit(value[i]))
       return false;
   }
   return true;
